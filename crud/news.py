1771766f5ef522b831c79b28373bb05a6b3b5934
mms <1605585155@qq.com>
2026-03-25 09:39:44 +0800

    新闻模块完善

--- crud/news.py
+++ crud/news.py
@@ -1,8 +1,6 @@
+from sqlalchemy import select, func, update
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select,func
-from models.news import Category,News
-
-
+from models.news import Category, News
 async  def get_categories(db: AsyncSession,skip: int=0,limit: int=100):
     stmt = select(Category).offset(skip).limit(limit)
     result=await db.execute(stmt)
@@ -18,3 +16,36 @@ async def get_news_count(db: AsyncSession,category_id: int):
     stmt=select(func.count(News.id)).where(News.category_id==category_id)
     result =await db.execute(stmt)
     return result.scalar_one()
+async def get_news_detail(db: AsyncSession,news_id: int):
+    stmt = select(News).where(News.id==news_id)
+    result = await db.execute(stmt)
+    return result.scalar_one_or_none()
+async def increase_news_views(db: AsyncSession,news_id: int):
+    stmt=update(News).where(News.id==news_id).values(views=News.views+1)
+    result=await db.execute(stmt)
+    await db.commit()
+    return result.rowcount>0
+
+async def get_related_news(db: AsyncSession, news_id: int, category_id: int, limit: int = 5):
+    # order_by 排序 → 浏览量和发布时间
+    stmt = select(News).where(
+        News.category_id == category_id,
+        News.id != news_id
+    ).order_by(
+        News.views.desc(),  # 默认是升序，desc 表示降序
+        News.publish_time.desc()
+    ).limit(limit)
+    result = await db.execute(stmt)
+    # return result.scalars().all()
+    related_news = result.scalars().all()
+    # 列表推导式 推导出新闻的核心数据，然后再 return
+    return [{
+        "id": news_detail.id,
+        "title": news_detail.title,
+        "content": news_detail.content,
+        "image": news_detail.image,
+        "author": news_detail.author,
+        "publishTime": news_detail.publish_time,
+        "categoryId": news_detail.category_id,
+        "views": news_detail.views
+    } for news_detail in related_news]
--- routers/news.py
+++ routers/news.py
@@ -5,6 +5,7 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from crud import news
 from config.db_conf import get_db
+from fastapi.exceptions import HTTPException
 #创建apirouter 实例
 
 """
@@ -28,9 +29,9 @@ async def get_categories(db: AsyncSession=Depends(get_db),skip: int=0,limit: int
 
 @router.get("/list")
 async def get_news_list(
-        category_id: int =Query(default=...,alias="categoryId"),
+        category_id: int =Query(...,alias="categoryId"),
         page:int =1,
-        page_size:int =Query(default=10,alias="pageSize",le=100),
+        page_size:int =Query(10,alias="pageSize",le=100),
         db: AsyncSession=Depends(get_db)
 ):
     news_list=await news.get_news_list(db,category_id,(page-1)*page_size,page_size)
@@ -44,4 +45,33 @@ async def get_news_list(
             "total":total,
             "hasMore":has_more
         }
+    }
+
+@router.get("/detail")
+async def get_news_detail(news_id: int=Query(default=...,alias="id"),db: AsyncSession=Depends(get_db)):
+    news_detail=await news.get_news_detail(db,news_id)
+    if not news_detail:
+        raise HTTPException(status_code=404,detail="新闻不存在")
+    # if news_detail:
+    #     news_detail.views+=1
+    #     await db.commit()
+    views_res=await news.increase_news_views(db,news_id)
+    if not views_res:
+        raise HTTPException(status_code=404,detail="新闻不存在")
+    related_news= await news.get_related_news(db,news_detail.id,news_detail.category_id)
+    return {
+          "code": 200,
+          "message": "success",
+          "data": {
+            "id": news_detail.id,
+            "title": news_detail.title,
+            "content": news_detail.content,
+            "image": news_detail.image,
+            "author": news_detail.author,
+            "publishTime": news_detail.publish_time,
+            "categoryId": news_detail.category_id,
+            "views": news_detail.views,
+            "relatedNews": related_news
+          }
+
     }
